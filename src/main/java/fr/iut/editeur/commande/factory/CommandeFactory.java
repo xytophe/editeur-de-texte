486bfe7e51ecd0ff3b1887eb1fb96723cf504760
cordeillal <lucas.cordeilla@etu.umontpellier.fr>
2023-11-06 09:32:32 +0100

    feat: remplacer
    feat: ajout CommandeRemplacer.java
    
    feat: ajout case remplacer à createCommand

--- src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
+++ src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
@@ -1,6 +1,7 @@
 package fr.iut.editeur.commande.factory;
 
 import fr.iut.editeur.commande.*;
+import fr.iut.editeur.commande.invoker.CommandeRemplacer;
 import fr.iut.editeur.document.Document;
 
 public class CommandeFactory {
@@ -19,8 +20,8 @@ public class CommandeFactory {
     public Commande createCommand(String name, Document document, String[] parameters) {
         switch (name) {
             case "ajouter" : return new CommandeAjouter(document, parameters);
+            case "remplacer" : return new CommandeRemplacer(document, parameters);
             default: return null;
         }
     }
-
 }
---
+++ src/main/java/fr/iut/editeur/commande/invoker/CommandeRemplacer.java
@@ -0,0 +1,23 @@
+package fr.iut.editeur.commande.invoker;
+
+import fr.iut.editeur.commande.CommandeDocument;
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument  {
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 2) {
+            System.err.println("Format attendu : remplacer;depart;fin;chaine");
+            return;
+        }
+        int start = Integer.parseInt(parameters[1]);
+        int end = Integer.parseInt(parameters[2]);
+        this.document.remplacer(start, end, parameters[3]);
+        super.executer();
+    }
+}
