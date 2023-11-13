fb977922eb355a26dd44001e14209740a46d49db
xytophe <xytophe@proton.me>
2023-11-13 12:25:34 +0100

    docs: ajout doc pour les fonctions importantes

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -16,6 +16,10 @@ public class Document {
         this.texte = texte;
     }
 
+    /**
+     * Permet d'ajouter du texte au document
+     * @param texte texte à ajouter
+     */
     public void ajouter(String texte) {
         this.texte += texte;
     }
@@ -25,22 +29,41 @@ public class Document {
         return this.texte;
     }
 
+    /**
+     * Permet de remplacer un morceau du texte existant par un autre
+     * @param start index du caractère à partir du quel le texte doit être remplacer
+     * @param end index du caractère jusqu'où le texte doit être remplacer
+     * @param remplacement texte à mettre à la place de celui qui existe
+     */
     public void remplacer(int start, int end, String remplacement) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
         texte = leftPart + remplacement + rightPart;
     }
 
+    /**
+     * Permet de mettre un morceau de texte en majuscule
+     * @param start index du caractère à partir du quel le texte doit être mis en majuscule
+     * @param end index du caractère jusqu'où le texte doit être mis en majuscule
+     */
     public void majuscules(int start, int end) {
         String texteMaj = texte.substring(start, end);
         texteMaj = texteMaj.toUpperCase();
         remplacer(start, end, texteMaj);
     }
 
+    /**
+     * Permet de supprimer un morceau de texte du document
+     * @param start index du caractère à partir du quel le texte doit être supprimer
+     * @param end index du caractère jusqu'où le texte doit être supprimer
+     */
     public void effacer(int start, int end) {
         remplacer(start, end, "");
     }
 
+    /**
+     * Efface la totalité du texte du document
+     */
     public void clear() {
         texte = "";
     }
